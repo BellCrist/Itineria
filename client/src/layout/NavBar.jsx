ef2bbb0613b7f8752f3fccdb381fe20b86293426
cristianbellesi-uni <cristian.bellesi@studenti.unicam.it>
2026-05-29 23:25:45 +0200

    Riorganizzata la main nav bar in modalità desktop e mobile

--- client/src/layout/NavBar.jsx
+++ client/src/layout/NavBar.jsx
@@ -1,5 +1,6 @@
-import { useContext } from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { useContext, useState } from 'react';
+import { Collapse, Nav, Navbar } from 'react-bootstrap';
+import { Search } from 'react-bootstrap-icons';
 import Container from 'react-bootstrap/Container';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { NavLink, useNavigate } from 'react-router-dom';
@@ -14,6 +15,7 @@ function MainNavBar() {
     const navigate = useNavigate();
     const expand = "lg";
     const { user, logout } = useContext(AuthContext);
+    const [showMobileSearch, setShowMobileSearch] = useState(false);
 
     const handleLogin = () => {
         navigate('/login');
@@ -30,11 +32,15 @@ function MainNavBar() {
 
                     <div className="row w-100 align-items-center m-0">
 
-                        <div className="col-4 col-lg-3 d-flex align-items-center p-0">
+                        {/* 1. COLONNA SINISTRA: Hamburger (Mobile) / Logo (Desktop) */}
+                        <div className="col-3 d-flex align-items-center justify-content-start p-0">
+                            {/* Toggle visibile solo su mobile */}
                             <div className='d-lg-none'>
                                 <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
                             </div>
-                            <Navbar.Brand href="#">
+
+                            {/* Logo Desktop: visibile solo su schermi grandi, allineato a sinistra */}
+                            <Navbar.Brand href="#" className="d-none d-lg-block m-0">
                                 <img
                                     src={logoImg}
                                     width="auto"
@@ -45,40 +51,84 @@ function MainNavBar() {
                             </Navbar.Brand>
                         </div>
 
-                        {/** Blocco centrale con searchbar e link */}
-                        <div className="col-4 col-lg-6 d-flex flex-column align-items-center p-0">
 
-                            {/* SearchBar sempre visibile nella Navbar */}
-                            <div className="w-100 d-flex justify-content-center">
-                                <SearchBar />
+                        {/* 2. COLONNA CENTRALE: Logo (Mobile) / SearchBar + NavLinks (Desktop) */}
+                        <div className="col-6 d-flex flex-column align-items-center justify-content-center p-0">
+
+                            {/* Logo mobile al centro */}
+                            <Navbar.Brand href="#" className="d-lg-none m-0">
+                                <img
+                                    src={logoImg}
+                                    width="auto"
+                                    height="60"
+                                    className="d-inline-block align-top"
+                                    alt="Itineria"
+                                />
+                            </Navbar.Brand>
+
+                            {/* Blocco Desktop: SearchBar e Links visibili solo su schermi grandi */}
+                            <div className="w-100 d-none d-lg-flex flex-column align-items-center">
+                                <div className="w-100 d-flex justify-content-center">
+                                    <SearchBar />
+                                </div>
+                                <Nav className="justify-content-center mt-2">
+                                    <Nav.Link as={NavLink} className="nav-item mx-2" to="/">Home</Nav.Link>
+                                    <Nav.Link as={NavLink} className="nav-item mx-2" to="/itinerari">I nostri itinerari</Nav.Link>
+                                    <Nav.Link as={NavLink} className="nav-item mx-2" to="/about">About</Nav.Link>
+                                    <Nav.Link as={NavLink} className="nav-item mx-2" to="/contatti">Contatti</Nav.Link>
+                                </Nav>
                             </div>
-
-                            {/* Nav Links visibili SOLO in desktop (d-none d-lg-flex) */}
-                            <Nav className="d-none d-lg-flex justify-content-center">
-                                <Nav.Link as={NavLink} className="nav-item mx-2" to="/">Home</Nav.Link>
-                                <Nav.Link as={NavLink} className="nav-item mx-2" to="/itinerari">I nostri itinerari</Nav.Link>
-                                <Nav.Link as={NavLink} className="nav-item mx-2" to="/about">About</Nav.Link>
-                                <Nav.Link as={NavLink} className="nav-item mx-2" to="/contatti">Contatti</Nav.Link>
-                            </Nav>
                         </div>
 
-                        <div className="col-4 col-lg-3 text-end p-0 justify-content-end">
+
+                        {/* 3. COLONNA DESTRA: Lente + Utente (Mobile) / Utente (Desktop) */}
+                        <div className="col-3 d-flex justify-content-end align-items-center p-0 gap-2 gap-lg-3">
+
+                            {/* Lente d'ingrandimento, visibile solo su mobile, che può far collassare la barra di ricerca */}
+                            <div className="d-flex d-lg-none">
+                                <button
+                                    type="button"
+                                    onClick={() => setShowMobileSearch(!showMobileSearch)}
+                                    aria-controls="mobile-search-collapse"
+                                    aria-expanded={showMobileSearch}
+                                    style={{
+                                        background: 'none',
+                                        border: 'none',
+                                        cursor: 'pointer',
+                                        color: 'inherit',
+                                        padding: '0'
+                                    }}
+                                    aria-label="Cerca"
+                                >
+                                    <Search size={24} strokeWidth={2} />
+                                </button>
+                            </div>
+
+                            {/* Bottone Utente / Login: sempre visibile a destra */}
                             {user ? (
                                 <div className="d-inline-block">
                                     <UserProfileButton />
                                 </div>
-                            )
-                                :
-                                (<div className="d-none d-sm-inline-flex gap-2">
+                            ) : (
+                                <div className="d-none d-sm-inline-flex gap-2">
                                     <AccessButton onClick={handleLogin} />
                                 </div>
-                                )}
+                            )}
                         </div>
                     </div>
 
+                    {/* 5. TENDINA SEARCH BAR MOBILE */}
+                    <div className="w-100 d-lg-none px-3">
+                        <Collapse in={showMobileSearch}>
+                            <div id="mobile-search-collapse">
+                                <div className="py-2 w-100 d-flex justify-content-center">
+                                    <SearchBar />
+                                </div>
+                            </div>
+                        </Collapse>
+                    </div>
 
-
-                    {/** l'offcanvas che racchiude le voci della navbar nel menu laterale */}
+                    {/** OFF CANVAS (Menu laterale - Invariato) */}
                     <Navbar.Offcanvas
                         id={`offcanvasNavbar-expand-${expand}`}
                         aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
@@ -100,7 +150,6 @@ function MainNavBar() {
                                     <Nav.Link as={NavLink} className="nav-item" to="/contatti">Contatti</Nav.Link>
                                 </Nav>
                             </div>
-
                         </Offcanvas.Body>
                     </Navbar.Offcanvas>
 
