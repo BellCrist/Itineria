14248ea5649ffad194cabff9b15629e85500c173
cristianbellesi-uni <cristian.bellesi@studenti.unicam.it>
2026-06-06 00:56:31 +0200

    modifica allo script di inizializzazione db

--- init.sql
+++ init.sql
@@ -1,4 +1,7 @@
 -- Script di inizializzazione per assegnare i permessi corretti all'utente
 GRANT ALL PRIVILEGES ON itineria_db.* TO 'admin-user-itineria'@'%';
 GRANT ALL PRIVILEGES ON itineria_db.* TO 'admin-user-itineria'@'localhost';
+-- Aggiorna l'utente esistente per usare il plugin nativo compatibile con Node.js
+ALTER USER 'admin-user-itineria'@'%' IDENTIFIED WITH mysql_native_password BY 'llha23_gg#9Fa';
+
 FLUSH PRIVILEGES;
